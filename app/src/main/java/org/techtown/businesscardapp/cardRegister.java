e1a73256aa8c945031847940103259a17a1c50a4
ChangSeob-Lim <limchangsz@gmail.com>
2019-10-07 14:02:56 +0900

    저번에 만들었던걸로 일단 화면 추가했습니다.

---
+++ app/src/main/java/org/techtown/businesscardapp/cardRegister.java
@@ -0,0 +1,14 @@
+package org.techtown.businesscardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cardRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_register);
+    }
+}
---
+++ app/src/main/java/org/techtown/businesscardapp/information.java
@@ -0,0 +1,14 @@
+package org.techtown.businesscardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
