ac1fb6e530380ccf3d600ad102f92ff9775956e7
ChangSeob-Lim <limchangsz@gmail.com>
2019-10-08 11:39:04 +0900

    회원가입 정렬

--- app/src/main/java/org/techtown/businesscardapp/cardRegister.java
+++ app/src/main/java/org/techtown/businesscardapp/cardRegister.java
@@ -10,5 +10,6 @@ public class cardRegister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_register);
+        getSupportActionBar().hide();
     }
 }
