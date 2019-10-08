30dd9ef38db2d62d0f50d7f21f0d74cc8b61bfc2
ChangSeob-Lim <limchangsz@gmail.com>
2019-10-08 12:16:55 +0900

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit f1a00754edd00a99ff6ed46dae672852fde1c1fe, reversing
    changes made to ac1fb6e530380ccf3d600ad102f92ff9775956e7.

--- app/src/main/java/org/techtown/businesscardapp/MainActivity.java
+++ app/src/main/java/org/techtown/businesscardapp/MainActivity.java
@@ -3,103 +3,27 @@ package org.techtown.businesscardapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private TextView tv_id, tv_pass;
-    private Button btn_userName;
+    private TextView tv_id, tv_pass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn_UserName = (Button)findViewById(R.id.btn_UserName);
+        tv_id = (TextView)findViewById(R.id.tv_id);
+        tv_pass = (TextView)findViewById(R.id.tv_pass);
+
         Intent intent = getIntent();
-        String userName = intent.getStringExtra("userName");
-        btn_UserName.setText(userName);
+        String userID = intent.getStringExtra("userID");
+        String userPassword = intent.getStringExtra("userPassword");
 
-        btn_UserName.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MyInfoActivity.class);
-                startActivity(intent);
-            }
-        });
+        tv_id.setText(userID);
+        tv_pass.setText(userPassword);
 
-
-//        tv_id = (TextView)findViewById(R.id.tv_id);
-//        tv_pass = (TextView)findViewById(R.id.tv_pass);
-//
-//        Intent intent = getIntent();
-//        String userID = intent.getStringExtra("userID");
-//        String userPassword = intent.getStringExtra("userPassword");
-//
-//        tv_id.setText(userID);
-//        tv_pass.setText(userPassword);
-
-        btn_UserName.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new BackgroundTask().execute();
-            }
-        });
-
-
-    }
-    class BackgroundTask extends AsyncTask<Void, Void, String>
-    {
-        String target;
-        @Override
-        protected void onPreExecute(){
-            target = "http://yujinpark10.dothome.co.kr/CardList.php"; // 서버에 존재하는 php파일 초기화
-        }
-
-        @Override
-        protected String doInBackground(Void... voids) {
-            try {
-                URL url = new URL(target);
-                HttpURLConnection httpURLConnection =(HttpURLConnection)url.openConnection();
-                InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String temp;
-                StringBuilder stringBuilder = new StringBuilder();
-                while ((temp = bufferedReader.readLine()) != null){
-                    stringBuilder.append(temp + "\n");
-
-                }
-                bufferedReader.close();
-                inputStream.close();
-                httpURLConnection.disconnect();
-                return stringBuilder.toString().trim();
-            }
-            catch (Exception e){
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Intent intent = new Intent(MainActivity.this, MyInfoActivity.class);
-            intent.putExtra("userCardList", result );
-            MainActivity.this.startActivity(intent);
-        }
     }
 }
--- app/src/main/java/org/techtown/businesscardapp/ValidateRequest.java
+++ app/src/main/java/org/techtown/businesscardapp/ValidateRequest.java
@@ -8,12 +8,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ValidateRequest extends StringRequest {
-    // URL에 POST방식으로 파라미터들을 전송
-    // 아이디값이 이미 가입된 아이디인지 검증
+    // 서버 URL 설정
     final static private  String URL = "http://yujinpark10.dothome.co.kr/UserValidate.php";
     private Map<String, String> map;
 
     public ValidateRequest(String userID, Response.Listener<String> listener){
+        // userID을 URL로 보냄으로써 회원가입여부를 체크 해주는 부분
         super(Method.POST, URL, listener, null);
         map = new HashMap<>();
         map.put("userID",userID);
